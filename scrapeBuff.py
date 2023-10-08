b21fd4a32ea95f4ce6843841dac34e0ebf06d8fb
Caidora <blakeisawesome49@gmail.com>
2023-10-09 00:06:48 +1300

    Fixed pathing to buy divs and other misc tweaks

--- scrapeBuff.py
+++ scrapeBuff.py
@@ -21,56 +21,78 @@ def notify(title, text):
               osascript -e 'display notification "{}" with title "{}"'
               """.format(text, title))
 
-def obtainItems(request, driver, maximumFloat): # obtain list of 10 items wear values and prices for the links found in json file. Returns true if match is found
+def obtainItems(request, driver, maximumFloat, maximumPrice): # obtain list of 10 items wear values and prices for the links found in json file. Returns true if match is found
 
     driver.get(request) # driver configs
+    time.sleep(0.5)
     cookies = pickle.load(open("cookies.pkl", "rb")) # enable cookies
     for cookie in cookies:
         driver.add_cookie(cookie)
-
     isNamed = True
     try:
         print(driver.find_element(By.XPATH, '/html/body/div[7]/div/div[1]/div[2]/div[1]/h1').text,':\n')
     except NoSuchElementException:
         print('pass')
         isNamed = False
+    offset = 0
     for i in range(10):
-        try:
-            wear = driver.find_element(By.XPATH, '/html/body/div[6]/div/div[7]/table/tbody/tr[{}]/td[3]/div/div[1]/div[1]'.format(i+2))
-            price = driver.find_element(By.XPATH, '/html/body/div[6]/div/div[7]/table/tbody/tr[{}]/td[5]/div[1]/strong'.format(i+2)) #consistent html behavior across different item links for CS:GO
-        except NoSuchElementException:
-            print("could not locate item")
-            continue
-        weartext = float(''.join(c for c in wear.text if c.isdigit() or c=='.'))
-        print("Listing {}".format(i+1))
-        print(price.text)
-        print("Float:", weartext,"\n")
-        print(threading.active_count())
-        if weartext < maximumFloat:
-            notify("Buff Scraper", "An item has been found that matches your criteria: {} {} \n{}"
-                   .format(driver.find_element(By.XPATH, '/html/body/div[7]/div/div[1]/div[2]/div[1]/h1').text if isNamed else 'Unknown Name', "Listing {}".format(i+1), "Float: {}".format(weartext)))
-            purchase(driver, i)
+
+        while True:
+            try:
+                wear = driver.find_element(
+                    By.XPATH, '/html/body/div[6]/div/div[7]/table/tbody/tr[{}]/td[3]/div/div[1]/div[1]'.format(i+2+offset))
+                # consistent html behavior across different item links for CS:GO
+                price = driver.find_element(
+                    By.XPATH, '/html/body/div[6]/div/div[7]/table/tbody/tr[{}]/td[5]/div[1]/strong'.format(i+2+offset))
+            except NoSuchElementException:
+                print("could not locate item")
+                try:
+                    column = driver.find_element(
+                        By.XPATH, '/html/body/div[6]/div/div[7]/table/tbody/tr[{}]'.format(i+2+offset))
+                    objectclass = column.get_attribute("class")
+                    if objectclass[0] == "d":
+                        offset = offset+1
+                    continue
+                except NoSuchElementException:
+                    driver.get(request)
+                    time.sleep(0.5)
+                    continue
+            weartext = float(''.join(c for c in wear.text if c.isdigit() or c=='.'))
+            print("Listing {}".format(i+1))
+            print(price.text)
+            print("Float:", weartext,"\n")
+            print("Threads running:", threading.active_count())
+            price = float(price.text[1:])
+            print("checking if {} is less than {}".format(price,maximumPrice))
+            if (weartext < maximumFloat) and (price<maximumPrice):
+                notify("Buff Scraper", "An item has been found that matches your criteria:  {} \n{}"
+                       .format("Listing {}".format(i+1), "Float: {}".format(weartext)))
+                purchase(driver, i+offset)
+            break
     driver.quit
 
 class ScrapeThread(threading.Thread):
-    def __init__(self, scrapernumber, maximumFloat):
+    def __init__(self, scrapernumber):
         threading.Thread.__init__(self)
         self.scrapernumber = scrapernumber
-        self.maximumFloat = maximumFloat
     def run(self):
         scrapeCount = 'scraper' + str(self.scrapernumber)
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        driver = webdriver.Chrome(options=options)
+        time.sleep(1)
         while True:
-            for link in data[scrapeCount]:
-                options = webdriver.ChromeOptions()
-                options.add_argument('--headless')
-                driver = webdriver.Chrome(options=options)
-                obtainItems(link, driver, self.maximumFloat)
+            for dicto in data[scrapeCount]:
+                link = dicto["link"]
+                price = float(dicto["price"])
+                maximumFloat = float(dicto["float"])
+                obtainItems(link, driver, maximumFloat, price)
 
-def scrape(firstScraper, lastScraper, maximumFloat):
+def scrape(firstScraper, lastScraper):
     threads = []
 
     for scrapeNum in range(firstScraper, lastScraper+1):
-        t = ScrapeThread(scrapeNum, maximumFloat)
+        t = ScrapeThread(scrapeNum)
         t.start()
         threads.append(t)
     
@@ -87,6 +109,6 @@ else:
     scrape(int(sys.argv[1]), int(sys.argv[2]), float(sys.argv[3]))
 
 # Note: if you want an example of a function run, uncomment this:
-# scrape(1, 4, 0.10)
+scrape(1, 2)
 
 # add readme.md
